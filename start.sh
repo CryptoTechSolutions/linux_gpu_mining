cba72dba85c7bb125845dd47cb50f98a210663c0
CryptoTechSolutions <36574304+CryptoTechSolutions@users.noreply.github.com>
2018-02-17 15:38:03 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  us.hushmine.pro --user t1RAzJVsxr2bASMk7PJQ4hpAut4R9v8nBSj.CTech/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
